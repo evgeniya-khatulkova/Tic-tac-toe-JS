d6a80f454f9a0013058a69a9e772e750a4d67ed3
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2024-06-20 11:29:48 +0200

    add 4 functions for player, game board, game flow and interface. Game flow functions are checking game status and board status

---
+++ script.js
@@ -0,0 +1,72 @@
+function gameBoard(){
+  const gameboard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  return {gameboard}
+}
+
+function player(name, symbol){
+  const playersCombination = [];
+  let winner = false;
+  let active = false;
+
+  return {name, playersCombination,  winner, active, symbol}
+}
+
+function interface(){
+  const game = gameFlow();
+  const sections = [...document.querySelectorAll('.section')];
+
+  let activeSection = null;
+
+  sections.forEach(section => section.addEventListener('click', (event) => {
+      activeSection = +event.currentTarget.dataset.number;
+      [game.player1, game.player2].forEach(person =>{
+          person.active = !person.active;
+      });
+      if(game.player1.active) {
+          const reply = () => game.check(activeSection, game.player1);
+          if(reply()) {event.currentTarget.innerText = game.player1.symbol };
+      } else {
+          const reply = () => game.check(activeSection, game.player2);
+          if(reply()) {event.currentTarget.innerText = game.player2.symbol };
+      }
+  }));
+}
+
+function gameFlow(){
+  const player1 = player(prompt("Set the name of the first player"), "X");
+  const player2 = player(prompt("Set the name of the second player"), "0");
+  player1.active = true;
+  const available = gameBoard();
+  const winningCombination = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+
+  const check = (choice, player) => {
+      if(available.gameboard.includes(choice)){
+          const index = available.gameboard.indexOf(choice);
+          available.gameboard.splice(index, 1);
+          player.playersCombination.push(choice);
+          // checkWinCombination(player);
+          return true
+      } else {
+          console.log('wrong number');
+          return false;
+      }
+ }
+
+  const checkWinCombination = (player) => {
+      if(player.playersCombination.length > 2 && winningCombination.some(subarray => subarray.every(el => player.playersCombination.includes(el)))) {
+          console.log('win');
+          player.winner = true;
+          return true;
+      } else {
+          return false
+      }
+  }
+  return{player1, player2, check}
+}
+
+interface();
+
+// 1. make move
+// 2. check available
+// 3. add to players combination
+// 4. check the combination
