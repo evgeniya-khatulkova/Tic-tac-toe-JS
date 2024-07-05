1d7dd8507b888e3942e9f0dc04d4d9332209821f
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2024-07-05 17:07:31 +0200

    refactor application to store data/handle data using dom elements only

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-function interface(){
+/// js /////////////////////////////////////////////////////
+
+(function interface(){
   const sections = [...document.querySelectorAll('.section')];
-  const {player1, player2, check, checkWinCombination, checkTie} = gameFlow();
+  const {player1, player2, assignMove, checkWinCombination, checkTie} = gameFlow();
   gameboardReset();
 
   sections.forEach(section => section.addEventListener('click', (event) => {
-      if(!player1.winner && !player2.winner){
+      if(!player1.winner && !player2.winner && !event.currentTarget.classList.contains('selected')){
+          section.classList.add('selected');
           [player1, player2].forEach(person =>{
               if(person.active){
                   handleMove(person, event)
@@ -16,32 +19,33 @@ function interface(){
 
   function handleMove(person, event){
       const activeSection = +event.currentTarget.dataset.number;
-      const reply = check(activeSection, person);
-      if(reply){
-          {event.currentTarget.innerHTML = person.symbol };
+      assignMove(activeSection, person);
+          {event.currentTarget.innerHTML = `<span>${person.symbol}</span>` };
           person.symbol === "X" ? playerDisplay(player2.name) : playerDisplay(player1.name);
           if(checkTie()) {output.innerText = "It is a draw" };
           winnerDisplay(checkWinCombination(person));
-      }
   }
 
   function gameboardReset(){
-      sections.forEach(section => section.innerHTML = "");
+      sections.forEach(section => {
+          section.innerHTML = "";
+          section.classList.remove('selected');
+  });
       playerDisplay(player1.name);
   }
 
   function playerDisplay(name){
-      output.innerText = `Next turn is for: ${name}`;
+      output.innerText = `It is ${name}'s turn`;
   }
 
   function winnerDisplay(isWinner){
       if(isWinner) {output.innerText = player1.winner ? `${player1.name} won` : `${player2.name} won`};
   }
-}
+});
 
 function gameBoard(){
-  const gameboard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  return {gameboard}
+  let sectionsAvailable = 9;
+  return {sectionsAvailable}
 }
 
 function player(name, symbol){
@@ -56,18 +60,13 @@ function gameFlow(){
   const player1 = player(prompt("Set the name of the first player"), "X");
   const player2 = player(prompt("Set the name of the second player"), "0");
   player1.active = true;
-  const { gameboard } = gameBoard();
+  let { sectionsAvailable } = gameBoard();
 
   const winningCombination = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
 
-  const check = (choice, player) => {
-      if(gameboard.includes(choice)){
-          const index = gameboard.indexOf(choice);
-          gameboard.splice(index, 1);
+  const assignMove = (choice, player) => {
           player.playersCombination.push(choice);
-          return true
-      }
-      return false;
+          sectionsAvailable--;
  }
 
   const checkWinCombination = (player) => {
@@ -79,8 +78,8 @@ function gameFlow(){
   }
 
   const checkTie = () => {
-      return gameboard.length === 0;
+      return sectionsAvailable === 0;
   }
 
-  return{player1, player2, check, checkWinCombination, checkTie}
+  return{player1, player2, assignMove, checkWinCombination, checkTie}
 }
